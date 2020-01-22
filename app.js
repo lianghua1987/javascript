0ac4ec7102fa47fa773618461178349defc4ab18
Hua Liang <hualiang@mercuria.com>
2020-01-22 17:16:37 +0800

    ajax

--- app.js
+++ app.js
@@ -1,120 +1,47 @@
-class Book{
-    constructor(title, author, isbn){
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
-}
+document.getElementById('button').addEventListener('click', loadData);
 
-class UI {
-    addBookToList(book){
-        const list = document.getElementById('book-list');
-        // create element
-    
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
-            <td><a href = \'#\' class = \'delete\'>x</a></td>
-        `;
-        list.appendChild(row);        
-    }
+function loadData(){
 
-    alert(message, className){
-        const div = document.createElement('div');
-        div.className = `alert ${className}`;
-        div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.container');
-        const form = document.querySelector('#book-form');
-        container.insertBefore(div, form);    
-    }
+    // create an XHR object
 
-    
+    const xhr = new XMLHttpRequest();
 
-    clear(){
-        document.getElementById('title').value = '';
-        document.getElementById('author').value = '';
-        document.getElementById('isbn').value = '';
-    }
+    // OPEN
+    xhr.open('GET', 'data.txt', true);
 
-    delete(target){
-        if(target.className === 'delete'){
-            target.parentElement.parentElement.remove();
-            this.alert('book removed', 'success');
-        }
-    }
-}
+   
 
-class Storage{
-    static add(book){
-        const booklist = Storage.get();
-        booklist.push(book);
-        localStorage.setItem('books', JSON.stringify(booklist));        
-    }
-    static remove(isbn){
-        const booklist = Storage.get();
-        booklist.forEach(function(b, index){
-            if(b.isbn === isbn){
-                booklist.splice(index, 1);
-            }
-        });
-        localStorage.setItem('books', JSON.stringify(booklist));        
-
-    }
-    static get(){
-        const books = localStorage.getItem('books');
-        if(books !== null){
-            return JSON.parse(books);
-        } else {
-            return [];
+    xhr.onload = function(){
+        if(this.status === 200){
+          
         }
     }
 
-    static display(){
-        const booklist = Storage.get();
-        const ui = new UI();
-        booklist.forEach(function(e){
-            ui.addBookToList(e);
-        });
-    
+    // optional - used for spinner/loader
+
+    xhr.onprogress = function(){
+        console.log('ready state: ', xhr.readyState, this.responseText);
     }
+
+    xhr.onerror = function(){
+        console.log('request error.....')
+    }
+
+    // ready state values
+    // 0: request not init
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+    xhr.onreadystatechange = function(){
+        console.log('ready state: ', xhr.readyState, this.responseText);
+        if(this.status === 200 && this.readyState === 4){
+            document.getElementById('output').innerHTML = `<h3>${this.responseText}</h3>`;
+        }
+    }
+
+
+
+
+    xhr.send();
 }
-
-document.addEventListener('DOMContentLoaded', Storage.display);
-
-// Event listener
-document.getElementById("book-form").addEventListener('submit', function(e){    
-
-    // get form values
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const isbn = document.getElementById('isbn').value;
-    const book = new Book(title, author, isbn);
-
-
-    const ui = new UI();
-
-    if(title === '' || author === '' || isbn === ''){
-        ui.alert('please fill in the fileds', 'error');
-    } else {
-        // add book
-        ui.alert('book added', 'success');
-        ui.addBookToList(book);
-        Storage.add(book);
-        ui.clear();
-    }
-    
-    //set timeout
-    setTimeout(function(){
-        document.querySelector('.alert').remove();
-    }, 3000)
-    e.preventDefault();
-})
-
-document.getElementById('book-list').addEventListener('click', function(e){
-    const ui = new UI();
-    ui.delete(e.target);
-    Storage.remove(e.target.parentElement.previousElementSibling.textContent);
-    e.preventDefault();
-})
